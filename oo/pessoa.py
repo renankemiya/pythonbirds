40938de3d320a67971ef20d2045327986fe33081
renankemiya <renankemiya@gmail.com>
2020-07-10 17:07:04 -0300

    Criado e removido atributo dinâmico de objetos do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -3,6 +3,7 @@ class Pessoa:
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
@@ -16,3 +17,7 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
+    del luciano.filhos
+    luciano.sobrenome = 'Ramalho'
+    print(luciano.__dict__)
+    print(renzo.__dict__)
